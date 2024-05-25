02ef6150ebdb925559b4b162c5f2fb297971dc08
Alexey Nebolsin <alexey.nebolsin.jr@gmail.com>
2024-05-25 12:23:58 +0500

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "./components/ui/card";
+import { Button } from "./components/ui/button";
+import { Check } from "lucide-react";
+
+function YesComponent() {
+    const Component = React.forwardRef((props, ref) => <Check ref={ref as any} />);
+
+    const MotionComponent = motion(Component);
+
+    return (
+        <div>
+            <CardHeader>
+                <CardTitle>You are a great person</CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col justify-center items-center h-24">
+                <MotionComponent
+                    animate={{
+                        width: 64,
+                        height: 64,
+                    }}
+                />
+            </CardContent>
+        </div>
+    );
+}
+
+interface Offset {
+    x: number;
+    y: number;
+}
+
+export function App() {
+    const [offset, setOffset] = useState<Offset>({
+        x: 0,
+        y: 0,
+    });
+    const [clickedYes, setClickedYes] = useState(false);
+
+    const maxXOffset = window.screen.availWidth / 2 - 200;
+    const maxYOffset = window.screen.availHeight / 2 - 200;
+
+    const onHoverNo = () => {
+        const newOffset: Offset = {
+            x: Math.random() * (maxXOffset * 2) - maxXOffset,
+            y: Math.random() * (maxYOffset * 2) - maxYOffset,
+        };
+        setOffset(newOffset);
+    };
+
+    return (
+        <div className="w-screen h-screen flex flex-col justify-center items-center bg-yellow-100">
+            <motion.div
+                initial={{ opacity: 1 }}
+                animate={{
+                    translateX: offset.x,
+                    translateY: offset.y,
+                }}
+            >
+                <Card className="w-[350px] bg-violet-200">
+                    {clickedYes ? (
+                        <YesComponent />
+                    ) : (
+                        <>
+                            <CardHeader>
+                                <CardTitle>Is Civ good?</CardTitle>
+                                <CardDescription>
+                                    Do you like the Sid Meier's Civilization game?
+                                </CardDescription>
+                            </CardHeader>
+                            <CardFooter className="flex gap-4">
+                                <Button variant="outline" onMouseEnter={onHoverNo}>
+                                    No
+                                </Button>
+                                <Button onClick={() => setClickedYes(true)}>Yes</Button>
+                            </CardFooter>
+                        </>
+                    )}
+                </Card>
+            </motion.div>
+        </div>
+    );
 }
 
 export default App;
